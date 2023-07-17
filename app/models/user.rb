b0234a84f3e29fd7606aac9df7fc3486a81f6339
nono2chan <hibiya.k0217@gmail.com>
2023-07-17 16:50:07 +0900

    デバイス一度削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  validates :profile, presence: true
-  validates :occupation, presence: true
-  validates :position, presence: true
+  # devise :database_authenticatable, :registerable,
+        #  :recoverable, :rememberable, :validatable
+  # validates :profile, presence: true
+  # validates :occupation, presence: true
+  # validates :position, presence: true
   # has_many :prototype, dependent: :destroy
   # has_many :comments, dependent: :destroy
   
