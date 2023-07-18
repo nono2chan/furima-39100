445bdfad2bc7e9c879241027881f76e990073a0c
nono2chan <hibiya.k0217@gmail.com>
2023-07-18 22:24:17 +0900

    単体テストコードを書く

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   # devise :database_authenticatable, :registerable,
         #  :recoverable, :rememberable, :validatable
   # validates :profile, presence: true
@@ -8,5 +12,14 @@ class User < ApplicationRecord
   # validates :position, presence: true
   # has_many :prototype, dependent: :destroy
   # has_many :comments, dependent: :destroy
-  
+  validates :nickname, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
+  validates :encrypted_password, presence: true
+  validates :family_name, presence: true
+  validates :first_name, presence: true
+  validates :family_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :birth_day, presence: true
+
 end
