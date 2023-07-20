42047f6570229d3e344d7905ba4828e0fc31e369
nono2chan <hibiya.k0217@gmail.com>
2023-07-20 14:59:21 +0900

    コード修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,17 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :items
-  has_many :comments
-  has_many :orders, through: :items
+  # has_many :items
+  # has_many :comments
+  # has_many :orders, through: :items
   
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  PASSWORD_REGEX_ZENKAKU = /\A[ぁ-んァ-ン一-龥]+\z/.freeze
+  PASSWORD_REGEX_ZENKAKU = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/.freeze
   PASSWORD_REGEX_KATAKANA = /\A[ァ-ヶー－]+\z/.freeze
 
   with_options presence: true do
     validates :nickname
-    validates :email
     validates :password, format: { with: PASSWORD_REGEX }
     validates :family_name, format: { with: PASSWORD_REGEX_ZENKAKU }
     validates :first_name, format: { with: PASSWORD_REGEX_ZENKAKU }
