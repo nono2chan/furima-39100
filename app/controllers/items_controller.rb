89e4ea2919efdabdb5713c9c1e96be4e2dcf1788
Kano Hibiya <130910475+nono2chan@users.noreply.github.com>
2023-07-24 15:50:45 +0900

    Revert "商品出品機能"

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,34 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
-  before_action :set_item, only: [:edit, :show, :update, :destroy]
-
   def index
-    # @items = Item.order('created_at DESC')
+    # @item = Item.new
+    # render index
   end
-
-  def new
-    @item = Item.new
-  end
-  
-  def create
-    @item = Item.new(item_params)
-    # @item = current_user.items.build(item_params)
-
-    # binding.pry
-    if @item.save
-      redirect_to root_path
-    else
-      render :new
-    end
-  end
-
-  private
-
-  def item_params
-    params.require(:item).permit(:name, :image, :price, :text, :genre_id, :quality_id, :payment_id, :prefecture_id, :days_id).merge(user_id: current_user.id)
-  end
-
-  # def set_item
-  #   @item = Item.find(params[:id])
-  # end
 end
--- app/models/purchase.rb
+++
@@ -1,9 +0,0 @@
-class Order < ApplicationRecord
-  belongs_to :user
-  belongs_to :item
-  has_one :destination
-
-  belongs_to :user
-  belongs_to :item
-  has_one :destination, dependent: :destroy
-end
