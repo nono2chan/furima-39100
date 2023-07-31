ee761eecd45f49419a12059aad160cfb86504059
nono2chan <hibiya.k0217@gmail.com>
2023-07-31 21:55:59 +0900

    修正

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -28,7 +28,7 @@ class PurchasesController < ApplicationController
   private
 
   def purchase_params
-    params.require(:purchase_destination).permit(:token, :post_code, :prefecture_id, :city, :address, :building_name, :phone_number, :item_id ).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:purchase_destination).permit(:token, :post_code, :prefecture_id, :city, :address, :building_name, :phone_number ).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 
   def pay_item
