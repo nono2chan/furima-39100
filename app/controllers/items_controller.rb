26a1f579257724659f4188a9590354e5906340fa
nono2chan <hibiya.k0217@gmail.com>
2023-07-31 20:49:49 +0900

    削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit]
-  before_action :get_item, only: [:edit, :show, :update]
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
+  before_action :get_item, only: [:edit, :show, :update, :destroy]
 
   def index
     @items = Item.includes(:purchase).order('items.created_at DESC')
@@ -37,7 +37,20 @@ class ItemsController < ApplicationController
       render :edit
     end
   end
- 
+
+  def destroy
+    if @item.user.id != current_user.id 
+      if @item.destroy
+        redirect_to root_path
+      else
+        render :show
+      end
+    else
+      redirect_to root_path
+    end
+  end
+
+  
   def show
   end
 
